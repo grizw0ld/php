0f8c226b973361c23adcab0a2823623f1b53dd78
Mark Grzechowiak <makgrz@gmail.com>
2013-01-24 22:37:16 -0500

    trying to commit

---
+++ 02/pursuevariables.php
@@ -0,0 +1,26 @@
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML  1.0 Transitional//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+ <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+ <head>
+   <meta http-equiv="content-type"  
+   content="text/html; charset=utf-8" />
+   <title>Pursue Variables</title>
+ </head>
+ <body>
+	<?php
+	/*
+	 *Filename: pursuevariables.php
+	 *Book reference: <Chapter 2 Pursue, PHP for the web>
+	 *Created by: Mark G
+	 * Jan 24th 2013
+	 */
+	 $phone_number = "555-2424";
+	 $last_name = "Smith";
+	 $first_name = "Jhonas";
+	 $age = 25;
+	 $gaa = 2.74;
+	 print "<b>$first_name $last_name</b> is a $age year old goaltender with a 
+	 $gaa goals against average";
+	?>
+ </body>
+ </html>
